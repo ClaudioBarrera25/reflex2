602e18c15aab2bfd0e0a4744376279914a16f260
ClaudioBarrera25 <105759307+ClaudioBarrera25@users.noreply.github.com>
2025-01-18 20:10:27 -0300

    Cambios de config

--- rxconfig.py
+++ rxconfig.py
@@ -8,6 +8,8 @@ from sqlalchemy import create_engine
 # Load environment variables from .env
 load_dotenv()
 
+railway_domain = "RAILWAY_PUBLIC_DOMAIN"
+
 # Fetch variables
 USER = os.getenv("user")
 PASSWORD = os.getenv("password")
@@ -31,5 +33,15 @@ config = rx.Config(
     app_name="reflex2",
     db_url=DATABASE_URL,
     api_url="http://reflex-production-eb39.up.railway.app:8000"
-    
+)
+
+config = rx.Config(
+    app_name="reflex2",
+    telemetry_enabled=False,
+    frontend_port=3000, # default frontend port
+    backend_port=8000, # default backend port
+    db_url=DATABASE_URL,
+    # api_url="http://reflex-production-eb39.up.railway.app:8000",
+    # use https and the railway public domain with a backend route if available, otherwise default to a local address
+    api_url=f'https://{os.environ[railway_domain]}/backend' if railway_domain in os.environ else "http://127.0.0.1:8000"
 )
