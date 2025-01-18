a06e52c5ea9d5c7808edc14c7871794b36041246
ClaudioBarrera25 <105759307+ClaudioBarrera25@users.noreply.github.com>
2025-01-18 14:45:35 -0300

    Añadir api_url

--- rxconfig.py
+++ rxconfig.py
@@ -19,14 +19,15 @@ DBNAME = os.getenv("dbname")
 DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 DATABASE_URL = f"postgresql://postgres:{PASSWORD}@db.tvwdfnmrkrlyalffpdiu.supabase.co:5432/postgres"
 DATABASE_URL = f"postgresql+psycopg2://postgres:{PASSWORD}@db.tvwdfnmrkrlyalffpdiu.supabase.co:5432/postgres?sslmode=require"
-DATABASE_URL = f"postgresql+psycopg2://postgres:Xb0uNqOYm6Ow04ni@db.tvwdfnmrkrlyalffpdiu.supabase.co:5432/postgres?sslmode=require"
+DATABASE_URL = f"postgresql+psycopg2://postgres:{PASSWORD}@db.tvwdfnmrkrlyalffpdiu.supabase.co:5432/postgres?sslmode=require"
 
-# DATABASE_URL = f"postgresql://postgres:{PASSWORD}@db.tvwdfnmrkrlyalffpdiu.supabase.co:6543/postgres"
+
 DATABASE_URL = f"postgresql://postgres.jlrjgnzyltqgotnqkuzd:{PASSWORD}@aws-0-us-west-1.pooler.supabase.com:6543/postgres"
 
 
 
 config = rx.Config(
     app_name="reflex2",
-    db_url=DATABASE_URL
+    db_url=DATABASE_URL,
+    api_url="http://reflex-production-eb39.up.railway.app/8000"
 )
